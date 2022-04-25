ff96affaacc021b036ad1b9e9f191dd6d811c059
Jose Delgado <jdelgado1025@gmail.com>
2022-04-25 13:16:27 -0700

    Added model for an individual stock

---
+++ StockMarket/Models/Stock.cs
@@ -0,0 +1,19 @@
+﻿namespace StockMarket.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+
+        public string? Symbol { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Currency { get; set; }
+
+        public string? Exchange { get; set; }
+
+        public string? Country { get; set; }
+
+        public string? Type { get; set; }
+    }
+}
