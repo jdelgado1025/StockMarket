a6c930d1bb3c16ebd1ce430bf40dc33e24554b20
Jose Delgado <jdelgado1025@gmail.com>
2022-04-25 13:21:57 -0700

    Added browser link to project

--- StockMarket/Program.cs
+++ StockMarket/Program.cs
@@ -12,6 +12,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+    app.UseBrowserLink();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
