ee83c67faa44806972ff733afc0dfff2f5234acb
Jose Delgado <jdelgado1025@gmail.com>
2022-04-25 14:50:14 -0700

    added implementation for data context file

--- StockMarket/Data/StockContext.cs
+++ StockMarket/Data/StockContext.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StockMarket.Models;
 
 namespace StockMarket.Data
 {
     public class StockContext :  DbContext
     {
+        public StockContext(DbContextOptions<StockContext> options) : base(options) { }
+
+        public DbSet<Stock> Stocks { get; set; }
     }
 }
