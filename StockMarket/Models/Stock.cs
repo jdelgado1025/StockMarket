df32ed8d0a5179c81d803fcc12d6d7beb5a2c8f5
Jose Delgado <jdelgado1025@gmail.com>
2022-05-03 22:16:16 -0700

    Changed fields to be non-nullable

--- StockMarket/Models/Stock.cs
+++ StockMarket/Models/Stock.cs
@@ -4,16 +4,16 @@
     {
         public int Id { get; set; }
 
-        public string? Symbol { get; set; }
+        public string Symbol { get; set; }
 
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
-        public string? Currency { get; set; }
+        public string Currency { get; set; }
 
-        public string? Exchange { get; set; }
+        public string Exchange { get; set; }
 
-        public string? Country { get; set; }
+        public string Country { get; set; }
 
-        public string? Type { get; set; }
+        public string Type { get; set; }
     }
 }
